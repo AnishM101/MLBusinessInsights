6f8397c7051770fccb50391820dff0905734fcad
AnishM101 <166966130+AnishM101@users.noreply.github.com>
2024-10-07 22:14:13 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, send_from_directory, redirect, url_for
+from BankChurnPrediction.app import churn_app
+from GDPTimeSeriesForecasting.app import time_app
+from GroceryStoreSalesPrediction.app import sales_app
+
+app = Flask(__name__, template_folder = '.')
+
+app.register_blueprint(churn_app, url_prefix = '/churn')
+app.register_blueprint(time_app, url_prefix = '/time')
+app.register_blueprint(sales_app, url_prefix = '/sales')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/styles.css')
+def home_styles():
+    return send_from_directory('.', 'styles.css')
+
+if __name__ == '__main__':
+    app.run(debug = True)
