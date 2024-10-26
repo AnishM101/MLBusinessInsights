c6199f49d83012cb0107eb46286daf4e73ea038f
AnishM101 <anish.manda@gmail.com>
2024-10-26 01:51:22 -0400

    js to flask app fix

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def home():
 def home_styles():
     return send_from_directory('.', 'styles.css')
 
+@app.route('/ChatBot/app.js')
+def chatbot_js():
+    return send_from_directory('ChatBot', 'app.js')
+
 @app.route('/chat', methods = ['POST'])
 def chat():
     user_input = request.json.get('message')
